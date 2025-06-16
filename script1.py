f16a58765dbb60b4078370bbb21a01512b182f8e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-16 15:19:25 -0600

    added render_templates and added a folder for our html files

--- script1.py
+++ script1.py
@@ -1,11 +1,11 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Home page"
+    return render_template("home.html")
 
 @app.route('/about/')
 def about():
